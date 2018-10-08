f67488bddc43259f887cedb7502e3ce4436e198a
chloe <chloekaczmarek0@gmail.com>
2018-10-08 09:57:09 +0200

    Suit of Fibonacci

--- src/TestFibonacci.java
+++ src/TestFibonacci.java
@@ -5,11 +5,20 @@ public class TestFibonacci {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		/**
+		 * Suit of Fibonacci
+		 */
+		
 		int val1, val2, val3;
 		val1 = 0;
 		val2 = 1;
 		val3 = val1 + val2;
 		
+		
+		/**
+		 * Ask the rank
+		 */
 		System.out.println("Choisissez un rang");
 		
 		Scanner questionUser = new Scanner(System.in);
@@ -20,6 +29,11 @@ public class TestFibonacci {
 		
 		questionUser.close();
 		
+		
+		/**
+		 * Condition of value
+		 */
+		
 		if (a > 0) {
 			System.out.println(0);
 		} if (a > 1) {
@@ -28,6 +42,10 @@ public class TestFibonacci {
 			System.out.println(1);
 		}
 
+		
+		/**
+		 * Loop "for" to calculate suit of Fibonacci
+		 */
 		for (int i = 3; i < a; i++) {
 			val1 = val2;
 			val2 = val3;
